5aff0d8aad913ddbe1d0aa779d926b763067719f
Larry Masullo <lmasullo@gmail.com>
2019-08-04 21:37:00 -0500

    2nd

--- letter.js
+++ letter.js
@@ -1,36 +1,28 @@
-//Get the guessed letter
-//let userLetter = process.argv[2];
-//console.log(userLetter);
+// Get the guessed letter
+// let userLetter = process.argv[2];
+// console.log(userLetter);
 
-
-function Letter (){
+function Letter() {
   this.isGuessed = function (guessedLetter, splitMovie) {
+    // let userChar = userChar;
+    const underscore = '_';
+    const guessed = false;
 
-    //let userChar = userChar;
-    let underscore = '_';
-    let guessed = false;
-
-
-    //Check if guessedLetter is in splitMovie
-    var found = splitMovie.find(function(element) {
-      return element === guessedLetter;
-    });
+    // Check if guessedLetter is in splitMovie
+    const found = splitMovie.find(element => element === guessedLetter);
     console.log(found);
 
-    //The letter is found
-    if(found != undefined){
-      return guessedLetter
-    //Letter not found, return underscore
-    }else{
-      return underscore;
+    // The letter is found
+    if (found != undefined) {
+      return guessedLetter;
+      // Letter not found, return underscore
     }
-    
-  }
-}//End Letter Constructor 
+    return underscore;
+  };
+} // End Letter Constructor
 
-//let newReq = new Letter(userLetter);
+// let newReq = new Letter(userLetter);
 
-//newReq.isGuessed();
-
+// newReq.isGuessed();
 
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,49 +1,43 @@
-//Import letter.js
+// Import letter.js
 const Letter = require('./letter');
 
 const letter = new Letter();
 
-//Word Consructor
-const Word = function (){
-  //this.randMovie = objMovie.movie;
-  //this.letter = objMovie.letter;
-  //console.log(randMovie);
-  //console.log(letter);
-  //slit the movie string into an array of charactersd
-  
-  //Create an object of guessed letters
-  let guessedLetters =  {};
-  
-  this.createWordArray = function(objMovie){
-    //Add the letter to the guessedLetters object
-    let guessedLetter = objMovie.letter;
-    console.log(guessedLetter)
+// Word Consructor
+const Word = function () {
+  // this.randMovie = objMovie.movie;
+  // this.letter = objMovie.letter;
+  // console.log(randMovie);
+  // console.log(letter);
+  // slit the movie string into an array of charactersd
+
+  // Create an object of guessed letters
+  const guessedLetters = {};
+
+  this.createWordArray = function (objMovie) {
+    // Add the letter to the guessedLetters object
+    const guessedLetter = objMovie.letter;
+    console.log(guessedLetter);
     guessedLetters[guessedLetter] = guessedLetter;
 
-    //Split the movie into an array of characters
-    let splitMovie = objMovie.movie.toLowerCase().split("");
-    console.log(splitMovie)
-    console.log(guessedLetters)
+    // Split the movie into an array of characters
+    const splitMovie = objMovie.movie.toLowerCase().split('');
+    console.log(splitMovie);
+    console.log(guessedLetters);
 
-    //Call Letter.js function
-    let checkLetter = letter.isGuessed(guessedLetter, splitMovie);
-    console.log(checkLetter)
+    // Call Letter.js function
+    const checkLetter = letter.isGuessed(guessedLetter, splitMovie);
+    console.log(checkLetter);
+  };
+};
 
-  }
-
-  
-}
-
-//Export the Word object to index.js
+// Export the Word object to index.js
 module.exports = Word;
 
+// Create a new Word Constructor object
+// let newWord = new Word(RandMovie);
 
+// console.log(RandMovie);
 
-//Create a new Word Constructor object
-//let newWord = new Word(RandMovie);
-
-//console.log(RandMovie);
-
-
-//Call the function in that object to create the array of characters
-//newWord.createWordArray();
+// Call the function in that object to create the array of characters
+// newWord.createWordArray();
